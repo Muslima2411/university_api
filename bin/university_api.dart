19a9b62a852524cb316e40acfc363a9b30d57dbb
muslima <muslima241105@gmail.com>
2023-11-27 11:23:15 +0500

    first commit

---
+++ bin/university_api.dart
@@ -0,0 +1,5 @@
+import 'package:university_api/university_api.dart' as university_api;
+
+void main(List<String> arguments) {
+  print('Hello world: ${university_api.calculate()}!');
+}
---
+++ test/university_api_test.dart
@@ -0,0 +1,8 @@
+import 'package:university_api/university_api.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
